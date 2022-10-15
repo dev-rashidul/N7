0085e98a8850ada45400d1a90dbace22c208ea2a
dev-rashidul <rashidul.contact@gmail.com>
2022-10-15 23:06:32 +0600

    Test

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,31 @@
 $(document).on("click", "ul li", function(){
     $(this).addClass("active").siblings().removeClass("active")
 })
+
+
+// Clients Slider
+
+$('.slider').slick({
+  slidesToShow: 3,
+  slidesToScroll: 3,
+  speed: 300,
+  slidesToShow: 1,
+  centerMode: true,
+  variableWidth: true,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  responsive: [
+    {
+      breakpoint: 768,
+      settings: {
+        slidesToShow: 2
+      }
+    },
+    {
+      breakpoint: 480,
+      settings: {
+        slidesToShow: 1
+      }
+    }
+  ]
+});
