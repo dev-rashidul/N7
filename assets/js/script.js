b0dfa7c211d59dd5611b55c55de2b410722f0fdd
dev-rashidul <rashidul.contact@gmail.com>
2022-10-23 19:11:17 +0600

    Updated

--- assets/js/script.js
+++ assets/js/script.js
@@ -116,3 +116,13 @@ window.onload = function(){
         }
       }
 }
+
+
+// Counter JS
+
+jQuery(document).ready(function ($) {
+  $(".counter").counterUp({
+    delay: 5,
+    time: 1000,
+  });
+});
